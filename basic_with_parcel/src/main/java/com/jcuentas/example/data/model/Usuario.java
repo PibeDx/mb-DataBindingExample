5684947cc565cb6aabcf266ce040fc3407e57991
José Cuentas Turpo <jcuentast@gmail.com>
2015-08-14 11:26:56 -0500

    Modulo de DataBinding con @Parcel recuperamos datos de una actividad al girar

---
+++ basic_with_parcel/src/main/java/com/jcuentas/example/data/model/Usuario.java
@@ -0,0 +1,39 @@
+package com.jcuentas.example.data.model;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.jcuentas.example.BR;
+
+import org.parceler.Parcel;
+
+/**
+ * Created by Jose Cuentas Turpo on 13/08/2015 - 02:37 PM.
+ * E-mail: jcuentast@gmail.com
+ */
+@Parcel
+public class Usuario extends BaseObservable {
+    public boolean visible = false;
+    public String nombre;
+
+    @Bindable
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+        notifyPropertyChanged(com.jcuentas.example.BR.visible);
+    }
+    @Bindable
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        notifyPropertyChanged(BR.nombre);
+    }
+
+
+}
